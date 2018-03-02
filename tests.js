d4f6b1a3d503fdd475b47bbebb595f53d1d1d068
Mirza Sisic <mirzasisic57@gmail.com>
2018-03-02 14:42:02 -0500

    Mocha and Chai

--- tests.js
+++ tests.js
@@ -1,14 +1,34 @@
-var chai   = require('chai');
-var assert = chai.assert;
-var Add    = require('../maths');
-var sinon  = require('sinon');
+var chai = require('chai')
+var should = require('chai').should();
+var expect = chai.expect;
+var assert = chai.asset;
+var Add = require('./UnitTests/math.js');
 
-describe('Add tests', funct()
+describe('Addition Tests', function()
 {
-  it('should return 3 when called with 1 and 2', function()
-  {
-    var result = Add(1, 2);
+  it('should return 3 when passed one and two', function()
+{
+    var numberOne = 1;
+    var numberTwo = 2;
+
     var expectedResult = 3;
-    assert.equal(expectedResult, result);
+
+    var actualResult = Add(numberOne, numberTwo);
+
+    actualResult.should.equal(expectedResult);
+    expect(actualResult).to.equal(expectedResult);
   });
+
+  it('should return 3 when passed one and two', function()
+  {
+      var numberOne = 1;
+      var numberTwo = 4;
+  
+      var expectedResult = 3;
+  
+      var actualResult = Add(numberOne, numberTwo);
+  
+      actualResult.should.equal(expectedResult);
+      expect(actualResult).to.not.equal(notExpectedResult);
+    });
 });
