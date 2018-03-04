62ace0dff58cce6d14c1a7383207e6b489b4ff6b
Mirza Sisic <mirzasisic57@gmail.com>
2018-03-04 10:13:13 -0500

    Sinon

--- tests.js
+++ tests.js
@@ -1,35 +1,31 @@
-var chai = require('chai')
-var should = require('chai').should();
-var expect = chai.expect;
-var assert = chai.asset;
-var Add = require('./UnitTests/math.js');
+const chai   = require('chai')
+const should = require('chai').should();
+const expect = chai.expect;
+const assert = chai.asset;
+const sinon  = require('sinon');
+const Add = require('./UnitTests/math.js');
 
 describe('Addition Tests', function()
 {
   it('should return 3 when passed one and two', function()
 {
-    var numberOne = 1;
-    var numberTwo = 2;
+  let numberOne = 1;
+  let numberTwo = 2;
 
-    var expectedResult = 3;
+  let logSpy = sinon.spy();
+  Add(numberOne, numberTwo, logSpy);
 
-    var actualResult = Add(numberOne, numberTwo);
-
-    actualResult.should.equal(expectedResult);
-    expect(actualResult).to.equal(expectedResult);
-    assert.equal(actualResult, expectedResult, "{test failed}")
+  logSpy.called.should.be.true;
+ 
   });
 
-  it('should return 3 when passed one and two', function()
-  {
-      var numberOne = 1;
-      var numberTwo = 4;
-  
-      var expectedResult = 3;
-  
-      var actualResult = Add(numberOne, numberTwo);
-  
-      actualResult.should.equal(expectedResult);
-      expect(actualResult).to.not.equal(notExpectedResult);
-    });
+
+  let numberOne = 1;
+  let numberTwo = 2;
+
+  let logSpy = sinon.spy();
+  Add(numberOne, numberTwo, logSpy);
+
+  logSpy.calledWith(3).should.be.true;
+
 });
